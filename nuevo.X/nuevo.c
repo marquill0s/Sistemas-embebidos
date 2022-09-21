afc83bb8d87c7cda894f672693dc5089f5ccb5cf
marquill0s <114098648+marquill0s@users.noreply.github.com>
2022-09-21 11:43:01 -0600

    cree un proyecto nuevo

---
+++ nuevo.X/nuevo.c
@@ -0,0 +1,14 @@
+/*
+ * File:   nuevo.c
+ * Author: Ing. Marcos Paz
+ *
+ * Created on 21 de septiembre de 2022, 11:42 AM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    //prueba
+    return;
+}
